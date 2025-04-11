e398a1e036e06a3edd818b6860311ac69f8416f9
Eknoors960 <sukhshinderna@trentu.ca>
2025-04-11 11:49:20 -0400

    Task1_Subtask5

--- Vehicle.java
+++ Vehicle.java
@@ -8,19 +8,18 @@ public abstract class Vehicle {
     public enum VehicleStatus { AVAILABLE, RESERVED, RENTED, MAINTENANCE, OUTOFSERVICE }
 
     public Vehicle(String make, String model, int year) {
-    	if (make == null || make.isEmpty())
-    		this.make = null;
-    	else
-    		this.make = make.substring(0, 1).toUpperCase() + make.substring(1).toLowerCase();
-    	
-    	if (model == null || model.isEmpty())
-    		this.model = null;
-    	else
-    		this.model = model.substring(0, 1).toUpperCase() + model.substring(1).toLowerCase();
-    	
+        this.make = capitalize(make);
+        this.model = capitalize(model);
         this.year = year;
         this.status = VehicleStatus.AVAILABLE;
         this.licensePlate = null;
+    }
+
+    private String capitalize(String input) {
+        if (input == null || input.isEmpty()) {
+            return null;
+        }
+        return input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
     }
 
     public Vehicle() {
@@ -32,21 +31,16 @@ public abstract class Vehicle {
     }
 
     public void setStatus(VehicleStatus status) {
-    	this.status = status;
+        this.status = status;
     }
 
     public String getLicensePlate() { return licensePlate; }
-
     public String getMake() { return make; }
-
-    public String getModel() { return model;}
-
+    public String getModel() { return model; }
     public int getYear() { return year; }
-
     public VehicleStatus getStatus() { return status; }
 
     public String getInfo() {
         return "| " + licensePlate + " | " + make + " | " + model + " | " + year + " | " + status + " |";
     }
-
 }
